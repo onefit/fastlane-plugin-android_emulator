4e9480eedcae2d73256472bfc0083aa59d22f476
Lars Werkman <lars@one.fit>
2020-09-24 16:32:31 +0200

    Calling ADB wait-for from System

--- lib/fastlane/plugin/android_emulator/actions/android_emulator_action.rb
+++ lib/fastlane/plugin/android_emulator/actions/android_emulator_action.rb
@@ -18,9 +18,9 @@ module Fastlane
 
         UI.message("Creating new emulator")
         FastlaneCore::CommandExecutor.execute(
-          command: "#{sdk_dir}/tools/bin/avdmanager create avd -n '#{params[:name]}' -f -k '#{params[:package]}' -d 'Nexus 5'",
-          print_all: true,
-          print_command: false
+            command: "#{sdk_dir}/tools/bin/avdmanager create avd -n '#{params[:name]}' -f -k '#{params[:package]}' -d 'Nexus 5'",
+            print_all: true,
+            print_command: false
         )
 
         UI.message("Override configuration")
@@ -40,7 +40,7 @@ module Fastlane
 
         UI.message("Starting emulator")
         system("LC_NUMERIC=C; #{sdk_dir}/tools/emulator @#{params[:name]} > /dev/null 2>&1 &")
-        sh("#{adb} -e wait-for-device")
+        system("#{adb} -e wait-for-device")
 
         until Actions.sh("#{adb} -e shell getprop init.svc.bootanim", log: false).include? "stopped" do
           sleep(5)
@@ -68,7 +68,7 @@ module Fastlane
 
       def self.example_code
         [
-          'android_emulator(
+            'android_emulator(
             location: "9.1808 48.7771",
             package: "system-images;android-24;google_apis;x86_64",
             demo_mode: true,
@@ -79,32 +79,32 @@ module Fastlane
 
       def self.available_options
         [
-          FastlaneCore::ConfigItem.new(key: :sdk_dir,
-                                       env_name: "ANDROID_SDK_DIR",
-                                       description: "Path to the Android SDK DIR",
-                                       default_value: Actions.lane_context[SharedValues::ANDROID_SDK_DIR],
-                                       optional: false,
-                                       verify_block: proc do |value|
-                                         UI.user_error!("No ANDROID_SDK_DIR given, pass using `sdk_dir: 'sdk_dir'`") unless value and !value.empty?
-                                       end),
-          FastlaneCore::ConfigItem.new(key: :package,
-                                      env_name: "AVD_PACKAGE",
-                                      description: "The selected system image of the emulator",
-                                      optional: false),
-          FastlaneCore::ConfigItem.new(key: :name,
-                                       env_name: "AVD_NAME",
-                                       description: "Name of the AVD",
-                                       default_value: "fastlane",
-                                       optional: false),
-          FastlaneCore::ConfigItem.new(key: :location,
-                                       env_name: "AVD_LOCATION",
-                                       description: "Set location of the the emulator '<longitude> <latitude>'",
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :demo_mode,
-                                       env_name: "AVD_DEMO_MODE",
-                                       description: "Set the emulator in demo mode",
-                                       is_string: false,
-                                       default_value: true)
+            FastlaneCore::ConfigItem.new(key: :sdk_dir,
+                                         env_name: "ANDROID_SDK_DIR",
+                                         description: "Path to the Android SDK DIR",
+                                         default_value: Actions.lane_context[SharedValues::ANDROID_SDK_DIR],
+                                         optional: false,
+                                         verify_block: proc do |value|
+                                           UI.user_error!("No ANDROID_SDK_DIR given, pass using `sdk_dir: 'sdk_dir'`") unless value and !value.empty?
+                                         end),
+            FastlaneCore::ConfigItem.new(key: :package,
+                                         env_name: "AVD_PACKAGE",
+                                         description: "The selected system image of the emulator",
+                                         optional: false),
+            FastlaneCore::ConfigItem.new(key: :name,
+                                         env_name: "AVD_NAME",
+                                         description: "Name of the AVD",
+                                         default_value: "fastlane",
+                                         optional: false),
+            FastlaneCore::ConfigItem.new(key: :location,
+                                         env_name: "AVD_LOCATION",
+                                         description: "Set location of the the emulator '<longitude> <latitude>'",
+                                         optional: true),
+            FastlaneCore::ConfigItem.new(key: :demo_mode,
+                                         env_name: "AVD_DEMO_MODE",
+                                         description: "Set the emulator in demo mode",
+                                         is_string: false,
+                                         default_value: true)
         ]
       end
 
